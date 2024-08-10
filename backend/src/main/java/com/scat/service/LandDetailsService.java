66fe33bac03df6b0ecaaa4bfec8c4532f7826754
shiyam <shiyam98as@gmail.com>
2024-08-10 12:29:36 +0530

    new

--- backend/src/main/java/com/scat/service/LandDetailsService.java
+++ backend/src/main/java/com/scat/service/LandDetailsService.java
@@ -8,4 +8,5 @@ public interface LandDetailsService {
     List<LandDetails> getAllLandDetails();
     void deleteLandDetails(Long id);
     List<LandDetails> getLandDetailsByUsername(String username);
+    void uploadLandDetials();
 }
