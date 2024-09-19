9660a0f12621872368e72c9a31f6d2a13191a418
shiyam <shiyam98as@gmail.com>
2024-09-19 15:23:18 +0530

    C: -Remove

--- backend/src/main/java/com/scat/MyApp.java
+++ backend/src/main/java/com/scat/MyApp.java
@@ -3,19 +3,15 @@ package com.scat;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import com.scat.service.StorageService;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class MyApp {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(MyApp.class, args);
 	}
 
