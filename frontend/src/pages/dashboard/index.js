9549d0ac90280d9233aee1ed8fdaa38771f438a0
shiyam <shiyam98as@gmail.com>
2024-09-30 16:20:34 +0530

    Merge branch 'shiyam' into dev

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,13 +1,4 @@
 export * from "@/pages/dashboard/Home";
-<<<<<<< HEAD
-export * from "@/pages/dashboard/profile";
-export * from "@/pages/dashboard/Tables";
-export * from "@/pages/dashboard/notifications";
-
-
-
-=======
 export * from "@/pages/dashboard/Profile";
 export * from "@/pages/dashboard/Tables";
 export * from "@/pages/dashboard/notifications";
->>>>>>> origin/madhan
