a0a25537a9d103d214df4341c5603f8d31f1d76d
shiyam <shiyam98as@gmail.com>
2024-11-08 12:02:21 +0530

    hello

--- frontend/src/pages/dashboard/profile.jsx
+++ frontend/src/pages/dashboard/profile.jsx
@@ -97,10 +97,7 @@ export function Profile() {
   // Fetch user profile details
   const fetchUserProfile = async (userId) => {
     try {
-
-      const token = localStorage.getItem('token');
       const response = await axios.get(`http://localhost:8080/users/get/${userId}`);
-      
       const userData = response.data;
       setFormData({
         username: userData.username,
