d38a964a0e6c87ea73b655bff26f53faa825fddc
shiyam <shiyam98as@gmail.com>
2024-11-08 12:18:56 +0530

    Long 1

--- backend/src/main/java/com/scat/security/WebSecurityConfig.java
+++ backend/src/main/java/com/scat/security/WebSecurityConfig.java
@@ -51,17 +51,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
                 .antMatchers(SecurityConstants.SIGNUP_URL, SecurityConstants.LOGIN_URL, "/users/forgot-password", "/users/reset-password").permitAll()
                 .antMatchers("/admin/login").permitAll()
-                .antMatchers("/users/crops/save", "/users/crops/all").permitAll()
-                .antMatchers("/crops/categories/get/all", "/crops/categories/add").permitAll()
+                .antMatchers("/users/crops/save", "/users/crops/all").permitAll() // Allow access to login without authentication
+                .antMatchers("/crops/category/get/all", "/crops/categories/add").permitAll() // Allow access to login without authentication
+//                .anyRequest().authenticated() // Require authentication for all other endpoints
+
                 .antMatchers("/users/admin/login").permitAll()
                 .antMatchers("/users/admin/create").hasRole("ADMIN")
                 .antMatchers("/users/admin/create").permitAll()
-                .antMatchers("/users/admin/**/**").permitAll()
-                .antMatchers("/roles/all").permitAll()
-                .antMatchers("/users/**").permitAll()
-                .antMatchers("/users/api/{email}").permitAll()
-                .antMatchers("/users/get/**").permitAll() // Ensure this line is accessible
-                .anyRequest().authenticated() // Any other requests require authentication
+                .antMatchers("/users/admin/**").permitAll()
+                .antMatchers("/users/admin/admin/{email}").permitAll()
+                .antMatchers("/users/create").permitAll()
+               .antMatchers("/roles/all").permitAll()  
+               .antMatchers("/users/admin/**").permitAll()
+
+                .antMatchers("/users/admin/role/login").permitAll()
+                .antMatchers("/users/admin/user/{email}").permitAll()
+                .antMatchers("/users/admin/roles/create").permitAll()
+                .antMatchers("/users/admin/roles/create").hasRole("ADMIN")              
+                .antMatchers("/users/admin/assign-role").hasRole("ADMIN")               
+                .antMatchers("/users/admin/fetch/all").permitAll()
+               .antMatchers("/admin/**").hasRole("ADMIN")
+             .antMatchers("/user/**").hasRole("USER")
+                                
+                .antMatchers("/users/land-details").hasRole("USER")
+                .antMatchers("/users/land-details/submit").permitAll()
+                .antMatchers("/**").permitAll()  
+//                .anyRequest().authenticated()
+              
             .and()
             .addFilter(new AuthenticationFilter(authenticationManagerBean(), jwtUtil, userRepository))
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
