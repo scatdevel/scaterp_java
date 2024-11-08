33f7855a5fc9f36fadb3e3dab4c5ac2c4471db2c
shiyam <shiyam98as@gmail.com>
2024-11-08 17:12:32 +0530

    in

--- backend/src/main/java/com/scat/controller/LandDetailsController.java
+++ backend/src/main/java/com/scat/controller/LandDetailsController.java
@@ -53,11 +53,9 @@ public class LandDetailsController {
     	{
     		return  ResponseEntity.notFound().build();
     	}
-    	
-    	
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<List<LandDetails>> getAllLandDetails() {
         List<LandDetails> landDetailsList = landDetailsService.getAllLandDetails();
         return new ResponseEntity<>(landDetailsList, HttpStatus.OK);
